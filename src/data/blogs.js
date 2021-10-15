b9be94bca6355f9dcf838c6ba1dd2b18e10f855f
Vikas Kukreti <vickydvlpr@gmail.com>
2021-10-15 16:45:11 +0530

    Added More Blogs to make list full

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -3,6 +3,7 @@ const blogs = [
     id: 1,
     title: "How to earn online ?",
     author: 1,
+    category: 1,
     content: `1. Freelancing
         Freelancing has always been a popular way to earn money online and the Internet has several options. There are several websites offering freelance tasks for people with varying skills. All you need to do is to create an account, browse through the listings, and apply for the task that suits you. Some websites may even require you to create a personal listing with the details of your skillset, so that interested clients can contact you directly. Outfiverr.com,  ..
         
@@ -13,6 +14,7 @@ const blogs = [
     id: 2,
     title: "The More Complete Guide to Master React — Part 1",
     author: 2,
+    category: 3,
     content: `Hey, my React friends!
     Are you a frontend developer and would like to start using React for your next project? Great! Welcome to the React family and you will probably find this guideline useful. However, if you have been using React for a while, you can still treat this guideline as a checklist for you.
     In this article, I have divided my journey to master React in several stages. It is a high level guideline about what you probably need to learn being a React developer at different stages. I’d recommend you to continue more research after reading this article.
@@ -23,6 +25,30 @@ const blogs = [
     Stage 4: Pick your UI framework
     Stage 5: Understand common React Patterns
     Stage 6: Strengthen your ES6 Knowledge`,
+  },
+  {
+    id: 3,
+    title: "Powering up India’s defence manufacturing",
+    author: 2,
+    category: 3,
+    content: `It takes a lot of grit, commitment and resolve for initiating and completing any major reform. Changing the status quo while meeting the competing aspirations of the stakeholders needs a fine balancing act. However, our government, under the able guidance of our Prime Minister Narendra Modi, has never hesitated to take strong decisions and undertake reforms which are beneficial and in the long-term interests of the nation.
+
+    Defence sector boost
+    
+    Self-reliance in defence has been the cornerstone of India’s defence production policy. The recent call for ‘Atmanirbhar Bharat’ has provided further impetus to realise this goal. Indian defence industry, primarily catering to the needs of the armed forces, has evolved with a diversified product mix and market.`,
+  },
+  {
+    id: 4,
+    title: "WHO revises its additional dose advisory, but India needs to do the relevant local research",
+    author: 3,
+    category: 4,
+    content: `he World Health Organisation that had earlier called for a complete moratorium on Covid vaccine booster doses until 2021 end, has this week recommended them for immunocompromised people. This is because of emerging data about their higher risk of breakthrough infections after standard immunisation. But the advisory also makes clear that it is intended for countries that have maximised two-dose coverage. With around 30% of adults double-vaccinated so far India has some way to go, even if second doses are really picking up pace now. What it must not delay is the immunological research that will enable making an informed decision about booster shots, for the new year.
+
+    Reportedly Biological E has sought approval to conduct the third-phase clinical trial of its Corbevax vaccine as a single-dose booster for those who are fully vaccinated with Covishield or Covaxin. Such investigations are important as trials elsewhere have indeed shown that mixing and matching can be preferable. For example, an NIH study of the three vaccines currently authorised in the US – Pfizer, Moderna, Johnson & Johnson – has tested as many as nine combinations of initial shots and boosters.
+    
+    Advanced investigations of this nature need to be built upon understanding whether and how protection against infection, if not hospitalisation and death, is fading in India over time. Variances may turn up between different vaccines, different age groups, different regions and these will be critical in prioritising boosters. Remember many of our health workers were fully vaccinated by February. Monitoring their antibody levels is of utmost importance.
+    
+    The importance of local research bears underlining. Israel and the US for example don’t use the same definitions of severe disease, and this may explain why a large number of double-dosed Israelis have been found to be hospitalised with Covid-19 but very few Americans. So borrowing the findings of either country could be inapt. With Covaxin of course, there are no international studies to rely on. Finally, all Indian serosurveys should disaggregate data by vaccination status from now on. In the UK the React study that tests 1 lakh blood swabs for Covid-19 antibodies every six weeks, has been able to place the varying protections enjoyed by the unvaccinated, partially vaccinated and fully vaccinated on a timeline, helping make the case for booster shots. Strong data builds strong policy. India must use the current disease lull to gird up research that will be a reliable guide at the right time.`,
   },
 ];
 
